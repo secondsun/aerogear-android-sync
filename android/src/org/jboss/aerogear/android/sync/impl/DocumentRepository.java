106e9415be3268e4f5b8bb636733e743f12cbfb4
Summers Pittman <secondsun@gmail.com>
2014-04-29 15:42:01 -0400

    Adding store

---
+++ android/src/org/jboss/aerogear/android/sync/impl/DocumentRepository.java
@@ -0,0 +1,46 @@
+package org.jboss.aerogear.android.sync.impl;
+
+
+import org.jboss.aerogear.android.datamanager.Store;
+import org.jboss.aerogear.android.sync.Repository;
+import org.jboss.aerogear.android.sync.SynchronizationException;
+import org.jboss.aerogear.android.sync.document.Document;
+
+/**
+ * This repository manages data which has been wrapped in a "Document" object.
+ * 
+ */
+public class DocumentRepository<T> implements Repository<Document<T>>{
+
+    private final Store<Document<T>> store;
+
+    public DocumentRepository(Store<Document<T>> store) {
+        this.store = store;
+    }
+    
+    @Override
+    public boolean canMerge(Document<T> remoteData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean hasChanges() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void merge(Document<T> remoteData) throws SynchronizationException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Document<T> getDelta(Document<T> remoteData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void force(Document<T> remoteData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
