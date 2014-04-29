4858740234794026567809e1e79bec26b40182ef
Summers Pittman <secondsun@gmail.com>
2014-04-29 16:08:24 -0400

    Addming merge test

--- android/test/org/jboss/aerogear/android/sync/impl/DocumentRepositoryTest.java
+++ android/test/org/jboss/aerogear/android/sync/impl/DocumentRepositoryTest.java
@@ -2,20 +2,24 @@ package org.jboss.aerogear.android.sync.impl;
 
 import org.jboss.aerogear.android.impl.datamanager.DefaultIdGenerator;
 import org.jboss.aerogear.android.impl.datamanager.MemoryStorage;
+import org.jboss.aerogear.android.sync.SynchronizationException;
 import org.jboss.aerogear.android.sync.document.Document;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DocumentRepositoryTest {
 
-    DocumentRepository<String> repo;
-    
+    private DocumentRepository<String> repo;
+    private MemoryStorage<Document<String>> store;
+
     @Before
     public void setUp() {
-        repo = new DocumentRepository<String>(new MemoryStorage<Document<String>>(new DefaultIdGenerator()));
+        store = new MemoryStorage<Document<String>>(new DefaultIdGenerator());
+        repo = new DocumentRepository<String>(store);
     }
- 
+
     /**
      * Test that an empty repository will take any document.
      */
@@ -24,5 +28,38 @@ public class DocumentRepositoryTest {
         boolean result = repo.canMerge(new Document<String>("Test String"));
         Assert.assertTrue(result);
     }
-    
+
+    /**
+     * Test merge
+     *
+     * @throws SynchronizationException if the merge fails. Which is won't since
+     * this is a test
+     */
+    @Test
+    public void testMerge() throws SynchronizationException {
+        Document<String> document = new Document<String>("Test String");
+        document.setId("1");
+        document.setRevision("2090111021");
+
+        repo.merge(document);
+
+        assertEquals(document, store.read("1"));
+        
+    }
+
+    /**
+     * Test that an trying to merge the same document twice will fail.
+     */
+    @Test
+    public void testWontMergeCurrentRevision() throws SynchronizationException {
+        Document<String> document = new Document<String>("Test String");
+        document.setId("1");
+        document.setRevision("2090111021");
+
+        repo.canMerge(document);
+        repo.merge(document);
+        boolean result = repo.canMerge(document);
+        Assert.assertFalse(result);
+    }
+
 }
