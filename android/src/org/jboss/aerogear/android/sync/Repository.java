1bd05f7c4c77b7bff12d3491852c4d00d48c148b
Summers Pittman <secondsun@gmail.com>
2014-05-02 11:04:17 -0400

    javascript

--- android/src/org/jboss/aerogear/android/sync/Repository.java
+++ android/src/org/jboss/aerogear/android/sync/Repository.java
@@ -1,5 +1,15 @@
 package org.jboss.aerogear.android.sync;
 
+/**
+ * A repository is a class which manages the state of synchronizable objects.
+ * Repositories should check objects sync status and make sure that operations
+ * won't lead to conflicts or incorrect data. They do not perform storage or
+ * retrieval operations on their own, but delegate to Stores, Pipes, etc where
+ * appropriate.
+ *
+ *
+ * @param <T> The Object type that this repository manages.
+ */
 public interface Repository<T> {
 
     /**
