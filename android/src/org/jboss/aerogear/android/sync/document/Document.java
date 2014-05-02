9f3186b3df24503e14bc668aaa3a5e0f37159f4c
Summers Pittman <secondsun@gmail.com>
2014-05-02 11:20:26 -0400

    javadoc

--- android/src/org/jboss/aerogear/android/sync/document/Document.java
+++ android/src/org/jboss/aerogear/android/sync/document/Document.java
@@ -4,18 +4,19 @@ import org.jboss.aerogear.android.RecordId;
 import org.jboss.aerogear.android.sync.impl.DocumentRepository;
 
 /**
- * 
- * A document is a collection of versioned meta-data for some object.
- * 
- * For a usagge example see {@link DocumentRepository}
- * 
- * @author summers
+ *
+ * A document is a collection of versioned meta-data for some object. Many of
+ * these fields will be provided by a third party source (a sync server for
+ * instance).
+ *
+ * For a usage example see {@link DocumentRepository}
+ *
  * @param <T> the wrapped data for the document.
  */
 public class Document<T> {
-    
+
     private final T content;
-    
+
     @RecordId
     private String id;
     private String revision;
@@ -25,35 +26,75 @@ public class Document<T> {
         this.content = object;
     }
 
+    /**
+     * The ID is unique global identifier for the document and its content.
+     *
+     * @return the current id.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * The ID is unique global identifier for the document and its content.
+     *
+     * @param id a new id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * The revision is a String which identifies the revision of the document
+     * and content. This will be generated and provided by a sync server.
+     *
+     * @return the current revision
+     */
     public String getRevision() {
         return revision;
     }
 
+    /**
+     * The revision is a String which identifies the revision of the document
+     * and content. This will be generated and provided by a sync server.
+     *
+     * @param revision a new revision
+     */
     public void setRevision(String revision) {
         this.revision = revision;
     }
 
+    /**
+     * The parent revision is the revision of the content before it was edited.
+     * This can be cloned on the client side from the revision field of the
+     * current document.
+     *
+     * @return the parent revision
+     */
     public String getParentRevision() {
         return parentRevision;
     }
 
+    
+    /**
+     * The parent revision is the revision of the content before it was edited.
+     * This can be cloned on the client side from the revision field of the
+     * current document.
+     *
+     * @param parentRevision a new parentRevision
+     */ 
     public void setParentRevision(String parentRevision) {
         this.parentRevision = parentRevision;
     }
 
+    /**
+     * The content is the content which is managed by the document and Repository.
+     * 
+     * 
+     * @return the current content.
+     */ 
     public T getContent() {
         return content;
     }
-    
-    
-    
-    
+
 }
